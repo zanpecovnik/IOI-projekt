7b1fe9f1ea379f101088275ebc8871bb148547e9
Zan Pecovnik <zan.pecovnik@sinergise.com>
2021-12-04 18:38:36 +0100

    add 5 overlay images

--- HandDetection.py
+++ HandDetection.py
@@ -2,13 +2,28 @@ import cv2
 import time
 import os
 
-wCam, hCam = 1280, 720
+camWidth, camHeight = 1280, 720
+imageDir = "images"
+
+overlayImageSize = 180
+overlayImage = cv2.imread(f'{imageDir}/square.jpg')
+overlayImagePositions = [
+    {'x': 120, 'y': 60},
+    {'x': 300, 'y': 440},
+    {'x': 520, 'y': 270},
+    {'x': 760, 'y': 150},
+    {'x': 890, 'y': 540}
+]
 
 cap = cv2.VideoCapture(0)
-cap.set(3, wCam)
-cap.set(4, hCam)
+cap.set(3, camWidth)
+cap.set(4, camHeight)
 
 while True:
     success, img = cap.read()
+
+    for position in overlayImagePositions:
+        img[position['y'] : position['y'] + overlayImageSize, position['x'] : position['x'] + overlayImageSize] = overlayImage
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
